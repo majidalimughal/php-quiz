beba6f1eecc6a057764e212a5e2f616a493e2187
Majid Ali <46532870+majidalimughal@users.noreply.github.com>
2019-01-30 23:36:26 +0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -7,7 +7,7 @@
     <title>Quiz System</title>
     <link rel="icon" href="assets/img/favicon.png">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/styles.min.css">
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
